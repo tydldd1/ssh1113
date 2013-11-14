fc1cefced609ebbbe93fc18bcea3d907f61d509a
tydldd <ru2013_1@163.com>
2013-11-14 09:05:51 +0800

    thread run方法
    thread run方法

---
+++ ssh0605/src/com/ru/javaExam/thread/ThreadRunMethod.java
@@ -0,0 +1,29 @@
+package com.ru.javaExam.thread;
+
+/**
+ * Description:如果使用
+ * User: NanChengRu
+ * Date: 13-11-13
+ * Time: 下午11:16
+ * JDK: 1.6
+ * version: 1.0
+ */
+public class ThreadRunMethod implements Runnable{
+
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p/>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
